40e1d7ad5e08b9356a5ecffffda7124f19987445
marthamutsvandiani <marthakuts@gmail.com>
2024-11-12 13:51:47 +0200

    Initial commit of new Spring Boot project

---
+++ src/test/java/com/example/martha/MarthaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.martha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarthaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
